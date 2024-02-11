d5ec04233ad4cb594aa5044cdf67b0bf9c077c68
Armin Veres <armin.veres@hotmail.com>
2024-02-11 11:51:09 +0100

    feat: enable zram

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -127,6 +127,8 @@ fonts.packages = with pkgs; [
   (nerdfonts.override { fonts = [ "Meslo" "Terminus" ]; } )
 ];
 
+  zramSwap.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
