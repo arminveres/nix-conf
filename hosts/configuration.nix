0d4624b36841642be62684abefc18d7f3a75666c
Armin Veres <armin.veres@hotmail.com>
2024-05-25 23:26:49 +0200

    feat: add cachix for binary caching

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -13,7 +13,22 @@
   };
 
   nix = {
-    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+
+      # NOTE(aver): enable nix-community binary caching
+      trusted-users = [ userSettings.username ];
+      extra-substituters = [
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
+
+
     optimise.automatic = true;
     gc = {
       automatic = true;
@@ -139,6 +154,7 @@
     lsb-release
     libnotify # enable notify-send
     networkmanagerapplet
+    cachix
   ];
 
   programs.hyprland = {
