c20d28837413ab6f3e728a3e9a0e321720f818fb
Armin Veres <armin.veres@hotmail.com>
2024-06-06 18:23:12 +0200

    build: update

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
 
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
 
+    # NOTE(aver): Don't use the default flake input, as it breaks when building with split-monitor-workspaces
     # hyprland.url = "github:hyprwm/Hyprland";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland-plugins = {
