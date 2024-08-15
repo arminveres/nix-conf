0d7f6e888d95830991f536cb10344614ace4b5ac
Armin Veres <armin.veres@hotmail.com>
2024-08-15 18:38:04 +0200

    feat: add amdgpu featuremask and no polkit confirmation

--- modules/core/gaming.nix
+++ modules/core/gaming.nix
@@ -29,8 +29,21 @@
       corectrl = {
         enable = true;
         gpuOverclock.enable = true;
+        gpuOverclock.ppfeaturemask = "0xffffffff";
       };
     };
+
+    security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+          if ((action.id == "org.corectrl.helper.init" ||
+               action.id == "org.corectrl.helperkiller.init") &&
+              subject.local == true &&
+              subject.active == true &&
+              subject.isInGroup("users")) {
+                  return polkit.Result.YES;
+          }
+      });
+    '';
 
     hardware.xone.enable = true;
   };
