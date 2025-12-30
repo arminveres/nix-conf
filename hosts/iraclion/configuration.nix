38c33bd9745e41bd48a1f6298e508f033b1b65c9
Armin Richard Veres <armin.veres@proton.me>
2025-12-30 15:56:41 +0100

    refactor: move state version to hosts

--- hosts/iraclion/configuration.nix
+++ hosts/iraclion/configuration.nix
@@ -5,6 +5,14 @@
 { pkgs, systemSettings, ... }:
 
 {
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
   imports = [ ../../modules/core ];
   docker.enable = true;
   home-manager.users.${systemSettings.username} = { neovim.enable = true; };
@@ -47,7 +55,6 @@
     variant = "";
   };
 
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs;
@@ -74,13 +81,5 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
 
 }
