d3f6b4739c8e8f2d0fbc5cac325b3edc3ffce004
Armin Richard Veres <armin.veres@proton.me>
2026-01-30 12:07:58 +0100

    refactor: move gui/desktop stuff into home module desktop

--- modules/home/modules/desktop.nix
+++ modules/home/modules/desktop.nix
@@ -1,7 +1,12 @@
-{ pkgs, lib, config, ... }: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
   options.ave = {
-    desktop.enable = lib.mkEnableOption
-      "enables Home-Manager Desktop module, which includes basic tools to work with a GUI based system.";
+    desktop.enable = lib.mkEnableOption "enables Home-Manager Desktop module, which includes basic tools to work with a GUI based system.";
   };
 
   config = lib.mkIf config.ave.desktop.enable {
@@ -21,43 +26,60 @@
         };
       };
 
+      # required for my tax software...
+      java = {
+        enable = true;
+        package = pkgs.openjdk17;
+      };
     };
-    home.packages = with pkgs; [
-      gnome-calculator
-      gnome-disk-utility
-      dconf
-      firefox # browser
-      alacritty # terminal
-      # ghostty  # TODO(aver): 18-07-2025 seems to have some weird loading issues
-      nautilus # filebrowser
-      baobab # disk usage
-      networkmanagerapplet # network tools
-      pavucontrol # audio system tray
-      solaar # logitech peripherals
-      thunderbird # email client
-      mission-center
-      libreoffice
-      kdePackages.okular
-      nextcloud-client
-      obsidian
-      signal-desktop
-      # vesktop
-      discord
-      drawio
-    ];
 
-    home.pointerCursor = {
-      gtk.enable = true;
-      name = "Adwaita";
-      package = pkgs.adwaita-icon-theme;
-      size = 24;
+    home = {
+      packages = with pkgs; [
+        gnome-calculator
+        gnome-disk-utility
+        dconf
+        firefox # browser
+        alacritty # terminal
+        # ghostty  # TODO(aver): 18-07-2025 seems to have some weird loading issues
+        nautilus # filebrowser
+        baobab # disk usage
+        networkmanagerapplet # network tools
+        pavucontrol # audio system tray
+        solaar # logitech peripherals
+        thunderbird # email client
+        mission-center
+        libreoffice
+        kdePackages.okular
+        nextcloud-client
+        obsidian
+        signal-desktop
+        # vesktop
+        discord
+        drawio
+      ];
 
+      pointerCursor = {
+        gtk.enable = true;
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+        size = 24;
+      };
+
+      sessionVariables = {
+        GTK_THEME = "Colloid-Orange-Dark";
+      };
+      file.".face".source = .assets/profile.jpg;
+      file.".config/face.jpg".source = .assets/profile.jpg;
     };
+
+    xdg.mime.enable = true;
 
     dconf = {
       enable = true;
       settings = {
-        "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+        };
       };
     };
 
@@ -65,11 +87,17 @@
       enable = true;
       theme = {
         name = "Colloid-Orange-Dark";
-        package = (pkgs.colloid-gtk-theme.override {
-          themeVariants = [ "orange" ];
-          colorVariants = [ "dark" ];
-          tweaks = [ "black" "rimless" "normal" ];
-        });
+        package = (
+          pkgs.colloid-gtk-theme.override {
+            themeVariants = [ "orange" ];
+            colorVariants = [ "dark" ];
+            tweaks = [
+              "black"
+              "rimless"
+              "normal"
+            ];
+          }
+        );
       };
       iconTheme = {
         name = "Adwaita";
