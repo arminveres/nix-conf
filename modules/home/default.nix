791a8bb8353c7b7e37b74c80aa63e4a21d7ee659
Armin Richard Veres <armin.veres@proton.me>
2026-01-30 12:14:07 +0100

    refactor: finalize hm

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,8 +1,11 @@
-{ inputs, pkgs, lib, systemSettings, ... }: {
-  home.username = "${systemSettings.username}";
-  home.homeDirectory = "/home/${systemSettings.username}";
-  home.stateVersion = "23.11";
-
+{
+  pkgs,
+  systemSettings,
+  overlays,
+  inputs,
+  ...
+}:
+{
   programs = {
     home-manager.enable = true;
     btop = {
@@ -15,33 +18,44 @@
     };
     yazi.enable = true;
 
-    java = {
+    nh = {
       enable = true;
-      package = pkgs.openjdk17;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake = "${systemSettings.homeDirectory}/nix-conf?submodules=1"; # sets NH_OS_FLAKE variable for you
     };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      tmux.enableShellIntegration = true;
+      # defaultCommand = "rg --hidden -l ''";
+      # defaultCommand = "fd --type f";
+      defaultOptions = [
+        "--height 40%"
+        "--layout=reverse"
+        "--border"
+      ];
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      options = [ "--cmd cd" ];
+    };
+
+    direnv = {
+      nix-direnv.enable = true;
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    ripgrep.enable = true;
+    fd.enable = true;
 
   };
 
   imports = [ ./modules ];
-
-  home.packages = with pkgs; [
-    eza
-    fastfetch
-    gh
-    tldr
-    fzf
-    ncdu
-    imagemagick # for converting stuff
-    stow
-
-    # TODO(aver): investigate utility
-    age
-    termshark
-    atac
-    sshs
-    portal
-    miro
-  ];
 
   xdg = {
     enable = true;
@@ -56,4 +70,48 @@
     # disable as it does not correctly start as requested
     tray.enable = false;
   };
+
+  nixpkgs.overlays = overlays;
+
+  home = {
+    username = systemSettings.username;
+    homeDirectory = systemSettings.homeDirectory;
+    # IMPORTANT: set this once and don’t change it casually.
+    stateVersion = "26.05"; # pick your HM release/state version
+    packages =
+      with pkgs;
+      [
+        nodejs
+
+        # eza
+        # fastfetch
+        # gh
+        # tldr
+        # fzf
+        # ncdu
+        # imagemagick # for converting stuff
+        # stow
+
+        # TODO(aver): investigate utility
+        # age
+        # termshark
+        # atac
+        # sshs
+        # portal
+        # miro
+      ]
+      ++ [
+        inputs.pwndbg.packages.${system}.pwndbg
+      ];
+  };
+
+  programs = {
+  };
+
+  # my modules
+  ave = {
+    neovim.enable = true;
+    zsh.enable = true;
+  };
+
 }
