478e6f45b0d256c7f35957d643c2e21416b3d2ae
Armin Richard Veres <armin.veres@proton.me>
2026-02-01 18:51:09 +0100

    refactor(zsh): finalize refactor
    Moved everything from classic dotfiles to home manager.

--- modules/home/modules/zsh.nix
+++ modules/home/modules/zsh.nix
@@ -4,18 +4,39 @@
   config,
   ...
 }:
+let
+  link = config.lib.file.mkOutOfStoreSymlink;
+in
 {
   options.ave.zsh.enable = lib.mkEnableOption "enables Home-Manager ZSH module";
 
   config = lib.mkIf config.ave.zsh.enable {
+    xdg.configFile = {
+      "zsh/functions.zsh" = {
+        source = link "${config.home.homeDirectory}/nix-conf/dotfiles/zsh/.config/zsh/modules";
+        recursive = true;
+      };
+    };
     programs.zsh = {
       enable = true;
-      # autocd = true;
-      # history = {
-      #   append = true;
-      #   expireDuplicatesFirst = true;
-      #   ignoreAllDups = true;
-      # };
+      autocd = true;
+      history = {
+        append = true;
+        expireDuplicatesFirst = true;
+        ignoreAllDups = true;
+        ignoreSpace = true;
+      };
+      setOptions = [
+        # Treat  the  `#', `~' and `^' characters as part of patterns for filename generation, etc.
+        # (An initial unquoted `~' always produces named directory expansion.)
+        # "EXTENDED_GLOB"
+        "GLOB_DOTS" # match hidden files
+        "NOMATCH" # generate an error if a pattern in the arg list is invalid
+        "INTERACTIVE_COMMENTS" # allow comments in interactive shell
+        "NO_CLOBBER" # do not allow truncating existing files e.g, with >, need to use >| instead
+        "APPEND_HISTORY"
+        "HIST_VERIFY" # show command with history expansion to user before running it, e.g., !x commands
+      ];
       syntaxHighlighting.enable = true;
       autosuggestion.enable = true;
 
@@ -115,7 +136,80 @@
       };
       # TODO(aver): Integrate into home-manager
       initContent = lib.mkAfter ''
-        source $ZDOTDIR/.zshrc.backup
+        # ==================================================================================================
+        # Sourcing plugins and custom scripts
+        # ==================================================================================================
+
+        # Useful Functions
+        source "$ZDOTDIR/modules/functions.zsh"
+
+        # Normal files to source
+        # Exports are needed before aliases
+        source "$ZDOTDIR/modules/vim_mode.zsh"
+        source "$ZDOTDIR/modules/git.zsh"
+
+        # Prompt
+        source "$ZDOTDIR/plugins/git-prompt.zsh/git-prompt.zsh"
+        source "$ZDOTDIR/modules/prompt.zsh"
+
+        # Plugins
+        # For more plugins: https://github.com/unixorn/awesome-zsh-plugins
+        source "$ZDOTDIR/plugins/zsh-autopair/zsh-autopair.plugin.zsh"
+
+        # ==================================================================================================
+        # Key-bindings
+        # ==================================================================================================
+        # bindkey -s '^v' "^U$ZDOTDIR/scripts/fzf_vim.sh^M"
+        # bindkey -s '^f' "^Ucdi^M"
+        # bindkey -s '^s' "^Utmux-sessionizer^M"
+        bindkey -s '^_' "^U$ZDOTDIR/scripts/conf.sh^M"
+
+        bindkey "^[[3~" delete-char
+        bindkey '^o' end-of-line
+        # bindkey -r "^d"
+        # bindkey -r "^u"
+
+        # search using a prefix, e.g., `cd` only searches history including cd
+        autoload -U up-line-or-beginning-search && zle -N up-line-or-beginning-search
+        autoload -U down-line-or-beginning-search && zle -N down-line-or-beginning-search
+        bindkey "^p" up-line-or-beginning-search # Up
+        bindkey "^n" down-line-or-beginning-search # Down
+
+        # Edit line in vim with ctrl-e:
+        autoload edit-command-line; zle -N edit-command-line
+        bindkey '^e' edit-command-line
+
+        # ================================================================================================
+        # Distro specifig setup
+        # ================================================================================================
+        if [[ "$(uname)" == "Linux" ]] && [[ -n "$DISTRO" ]]; then
+            case "$DISTRO" in
+            Ubuntu | Debian)
+                alias nala='sudo nala'
+                alias upd='sudo apt update && sudo apt upgrade'
+                [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"                   # This loads nvm
+                [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+                ;;
+            Fedora) # echo Fedora
+                alias dnf='sudo dnf'
+                alias din='dnf install'
+                alias dup='dnf upgrade'
+                alias doffup='dnf offline-upgrade download -y && dnf offline-upgrade reboot'
+                ;;
+            *) ;;
+            esac
+        fi
+
+        if [[ -n $SSH_CONNECTION ]]; then
+            # Set window name to hostname when in SSH
+            printf '\033k%s\033\\' "$(hostname -s)"
+            # Reset window name on exit
+            trap 'printf "\033k\033\\"' EXIT
+        fi
+
+        # ==================================================================================================
+        # END
+        # ==================================================================================================
         zsh_end_time=$(python3 -c 'import time; print(int(time.time() * 1000))')
         echo "Shell init time: $((zsh_end_time - zsh_start_time)) ms"
       '';
